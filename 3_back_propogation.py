ac9360f50e827f565f2c461376c6562b32dd2822
joe <joseph.maliszewski93@gmail.com>
2022-05-12 14:24:37 +0100

    Back prop

---
+++ 3_back_propogation.py
@@ -0,0 +1,26 @@
+'''
+Simple example of backpropagation
+'''
+
+import torch
+
+x = torch.tensor(1.0)
+y = torch.tensor(2.0)
+
+#weight
+w = torch.tensor(1.0, requires_grad=True)
+
+#forward pass 
+y_hat = w*x
+
+#compute the loss
+loss = (y_hat - y)**2
+print(loss)
+
+#backward pass (cal gradient dw/dloss)
+loss.backward()
+print(w.grad)
+
+
+
+
